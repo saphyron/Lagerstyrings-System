c90946458edc5d1026d1b8d6289c407c906d75a7
SpecAcad-Jakob <Jakob.Damkjer@Specialisterne.dk>
2025-10-29 14:39:40 +0100

    User-view, finally

---
+++ Lagerstyrings System/src/User/User.cs
@@ -0,0 +1,8 @@
+﻿namespace LagerstyringsSystem.src.User {
+    public class User {
+        public int Id { get; private set; }
+        public string Username { get; set; }
+        public string PasswordClear { get; set; }
+        public int AuthEnum { get; set; }
+    }
+}
